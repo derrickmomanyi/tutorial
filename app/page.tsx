4f60115b46440f2b5fadca0244039dce43d0a1d7
derrickmomanyi <rickydee38@gmail.com>
2025-05-29 12:08:36 +0300

    Whitespace

--- app/page.tsx
+++ app/page.tsx
@@ -16,10 +16,6 @@ export default function Home() {
           <li className="mb-2 tracking-[-.01em]">
             Dont get  started by editing{" "}
 
-
-
-
-            
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
               app/page.tsx
             </code>
